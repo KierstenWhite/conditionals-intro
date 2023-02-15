0a1ddf6359c73d0e1d9e126c36da573a01ef9338
Kiersten White <kierstenswhite@gmail.com>
2023-02-15 09:40:05 -0500

    Conditionals Exercise #1-4 from 2/15/23

---
+++ main.js
@@ -0,0 +1,60 @@
+//Exercise #1
+
+if (1+1 > 5){
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+if (4*5 <= 20){
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+if (6-2 >= 0){
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+//Exercise #2
+
+let likesDogs = true
+if (likesDogs = true){
+    console.log("You're a dog person!")
+} else {
+    console.log("You're a cat person!")
+}
+
+//Exercise #3
+
+const personOne = {
+    name: "Kiersten",
+    preferredLanguage: "English",
+}
+
+if (personOne.preferredLanguage === "English"){
+    console.log(`Hello, ${personOne.name}!`)
+} else if (personOne.preferredLanguage === "Spanish"){
+    console.log(`Hola, ${personOne.name}!`)
+} else if (personOne.preferredLanguage === "French"){
+    console.log(`Bonjour, ${personOne.name}!`)
+}
+
+//Exercise #4
+
+var grade = 88
+
+if (grade >= 0 && grade <= 69){
+    console.log("You got an F!")
+} else if (grade >= 70 && grade <= 76){
+    console.log("You got a D!")
+} else if (grade >= 77 && grade <= 84){
+    console.log("You got a C!")
+} else if (grade >= 84 && grade <= 92){
+    console.log("You got a B!")
+} else if (grade >= 93 && grade <= 100){
+    console.log("You got an A!")
+}
+
